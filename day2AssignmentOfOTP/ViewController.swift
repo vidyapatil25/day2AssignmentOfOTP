6b81183ccc855a6e469954dd85774a0fc857039d
Vidya <vidya@vidyas-imac.local>
2019-11-16 19:44:18 +0530

    Initial Commit

---
+++ day2AssignmentOfOTP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day2AssignmentOfOTP
+//
+//  Created by Vidya on 16/11/19.
+//  Copyright © 2019 Vidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
